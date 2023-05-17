1796956d1f8b2c871b954b310655e8af9d5bfb40
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-05-17 16:46:05 +0530

    Hard Coded Expenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (<div>
+        <div>17th March 2023</div>
+        <div>
+            <h2>Expenses</h2>
+            <div>Amount</div>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movie Rs 200</p>
+        </div>
+    </div>)
+}
+
+export default ExpenseItem;
