19911db96cdecac3afa3dae4130033c2fea0bda8
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-05-22 16:37:13 +0530

    Manipulating the DOM - React States

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,25 @@
-
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
-
 import './ExpenseItem.css';
 
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
 
-const ExpenseItem=(props) => {
-    const clickHandler=()=>{
-        console.log("Clicked!!!")
-    }
+    const clickHandler = () => {
+        setTitle('100$');
+        console.log(title); // Log the updated value of title
+    };
+
     const deleteExpenseHandler = () => {
-        // Remove the expense item from the DOM
         const expenseItem = document.querySelector('.expense-item');
         expenseItem.remove();
-      };
-    
+    };
+
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
                 <div>{props.location}</div>
             </div>
@@ -26,6 +27,6 @@ const ExpenseItem=(props) => {
             <button onClick={deleteExpenseHandler}>Delete Expense</button>
         </div>
     );
-}
+};
 
 export default ExpenseItem;
